d32aed8946709c58f1d5e3d302d812051ee14760
Trevor Sharp <sharp.trevor@me.com>
2023-08-27 23:22:13 +0000

    Switch to video streaming directly from Patreon

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,12 @@
 import dotenv from 'dotenv';
 import Fastify from 'fastify';
-import FastifyStatic from '@fastify/static';
-import { downloadNewEpisodes } from './services/downloadService';
 import { getPosts, searchForCampaign } from './services/patreonService';
 import { buildFeed } from './services/feedService';
 
 dotenv.config();
 
-const feeds = process.env.FEEDS?.split(',').map((feedId) => feedId.trim()) ?? [];
-
 const fastify = Fastify({
   logger: true,
-});
-
-fastify.register(FastifyStatic, {
-  root: '/app/content',
-  prefix: '/content/',
 });
 
 fastify.get('/', async (_, reply) => {
@@ -23,20 +14,8 @@ fastify.get('/', async (_, reply) => {
   return 'Patreon Podcast Feeds Is Up And Running';
 });
 
-fastify.get('/update', async (_, reply) => {
-  updateFeeds();
-  reply.code(200);
-  return 'Updating Feeds';
-});
-
 fastify.get<{ Params: { feedId: string } }>('/:feedId', async (request, reply) => {
-  const feedId = feeds.find((feed) => feed.toLowerCase() === request.params.feedId.toLowerCase());
-  if (!feedId) {
-    reply.code(404);
-    return 'Feed Not Found';
-  }
-
-  const campaign = await searchForCampaign(feedId);
+  const campaign = await searchForCampaign(request.params.feedId);
   if (!campaign) {
     reply.code(404);
     return 'Campaign Not Found';
@@ -45,33 +24,26 @@ fastify.get<{ Params: { feedId: string } }>('/:feedId', async (request, reply) =
   const posts = await getPosts(campaign.id);
 
   reply.code(200);
-  return buildFeed(request.hostname, campaign, posts);
+  return buildFeed(request.hostname, request.params.feedId, campaign, posts);
+});
+
+fastify.get<{ Params: { feedId: string; postId: string } }>('/:feedId/:postId', async (request, reply) => {
+  const campaign = await searchForCampaign(request.params.feedId);
+  if (!campaign) {
+    reply.code(404);
+    return 'Campaign Not Found';
+  }
+
+  const post = await getPosts(campaign.id).then((posts) => posts.find((post) => post.id === request.params.postId));
+  if (!post) {
+    reply.code(404);
+    return 'Post Not Found';
+  }
+
+  reply.redirect(302, post.downloadUrl);
 });
 
 fastify.listen({ port: 3000, host: '0.0.0.0' }, (err, address) => {
   if (err) throw err;
   console.log(`Server is now listening on ${address}`);
 });
-
-let currentlyUpdating = false;
-
-const updateFeeds = async () => {
-  if (currentlyUpdating) return;
-
-  currentlyUpdating = true;
-
-  for (let i = 0; i < feeds.length; i++) {
-    const campaign = await searchForCampaign(feeds[i]);
-    if (!campaign) throw `Could not find campaign for feed ${feeds[i]}`;
-
-    const posts = await getPosts(campaign.id);
-
-    await downloadNewEpisodes(posts);
-  }
-
-  currentlyUpdating = false;
-};
-
-setInterval(() => updateFeeds().catch((e) => console.error(e)), 10 * 60 * 1000);
-
-console.log(`Feeds - ${feeds.join(' / ')}`);
